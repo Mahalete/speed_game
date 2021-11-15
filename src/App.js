00fd1db7b7d567619abe6d13ad01e29ee8d876ce
BC-macair011 <mahialemu@gmail.com>
2021-11-15 13:01:16 +0200

    Made circles, with colors and on click score increases by 10

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+import React, { Component } from "react";
 import "./App.css";
+import Circle from "./Circle";
+import { circles } from "./circles";
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  state = {
+    score: 0,
+  };
+  clickHandler = () => {
+    this.setState({
+      score: this.state.score + 10,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>SpeedGame</h1>
+        <p>Your score:{this.state.score}</p>
+        <div className="circles">
+          {circles.map((c) => (
+            <Circle
+              key={c.id}
+              color={c.color}
+              id={c.id}
+              click={this.clickHandler}
+            />
+          ))}
+        </div>
+        <div>
+          <button>Start</button>
+          <button>Stop</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
