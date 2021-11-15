50067b4ddd365b6ae955bb3d696500724ef0cd81
BC-macair011 <mahialemu@gmail.com>
2021-11-15 14:02:36 +0200

    add highlight to circle

--- src/App.js
+++ src/App.js
@@ -3,21 +3,55 @@ import "./App.css";
 import Circle from "./Circle";
 import { circles } from "./circles";
 
+const getRndInteger = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
 class App extends Component {
   state = {
     score: 0,
+    current: 0,
   };
+
+  timer = undefined;
+  pace = 1500;
+
   clickHandler = () => {
     this.setState({
       score: this.state.score + 10,
     });
   };
 
+  nextCircle = () => {
+    let nextActive;
+
+    do {
+      nextActive = getRndInteger(1, 4);
+    } while (nextActive === this.state.current);
+
+    this.setState({
+      current: nextActive,
+    });
+
+    this.pace *= 0.95;
+    this.timer = setTimeout(this.nextCircle, this.pace);
+
+    console.log("active circle is ", this.state.current);
+  };
+
+  startHandler = () => {
+    this.nextCircle();
+  };
+
+  stopHandler = () => {
+    clearTimeout(this.timer);
+  };
+
   render() {
     return (
       <div>
         <h1>SpeedGame</h1>
-        <p>Your score:{this.state.score}</p>
+        <p>Your score: {this.state.score}</p>
         <div className="circles">
           {circles.map((c) => (
             <Circle
@@ -25,12 +59,13 @@ class App extends Component {
               color={c.color}
               id={c.id}
               click={this.clickHandler}
+              active={this.state.current === c.id}
             />
           ))}
         </div>
         <div>
-          <button>Start</button>
-          <button>Stop</button>
+          <button onClick={this.startHandler}>Start</button>
+          <button onClick={this.stopHandler}>Stop</button>
         </div>
       </div>
     );
